177d158600d0cfd1ed0049b5adb34e270a020c12
jorgeoliart <jorgeoliart.gmail.com>
2023-06-10 09:00:04 -0500

    Update fixes repeat string files

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let outputTest = "";
+    for (let i = 0; i < num; i++) {
+        outputTest += string;
+    }
+    return outputTest;
 };
 
 // Do not edit below this line
