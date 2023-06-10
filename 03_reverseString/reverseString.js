95b06295d4cdebe544ee1d39e0fe921fd419d6be
jorgeoliart <jorgeoliart.gmail.com>
2023-06-10 09:01:01 -0500

    Update fixed reverse string js file

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+// create the parameter for the function: a string
+const reverseString = function(inputString) {
+    // split a string into its characters, including blanks
+    let characters = inputString.split("");
+    
+    // reverse the elements/characters in the array
+    characters = characters.reverse();
+    
+    // join the reversed characters into a single string
+    let reversedString = characters.join('');
 
+    return reversedString;
 };
 
 // Do not edit below this line
