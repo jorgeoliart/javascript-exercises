7667b0478fc16f12a086364fe6c857e6874d911e
jorgeoliart <jorgeoliart.gmail.com>
2023-06-11 14:26:31 -0500

    Make function with nested loops and counting backwards

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,26 @@
-const removeFromArray = function() {
+/*const removeFromArray = function(array, num, num2) {
+    for (let element of array) {
+        if (element === num || element === num2) {
+            let index = array.indexOf(element);
+            array.splice(index, 1);
+        }
+    }
+    return array;
+};*/
 
-};
-
+const removeFromArray = function(array, ...removableElements) {
+    // make a first loop that interates through the removable args
+    for (let elementRemove of removableElements) {
+        // make a second loop that iterates through the array
+        // count backwards to avoid altering the indexes in the array
+        for (let j = array.length - 1; j >= 0; j--)  {
+            if (array[j] === elementRemove) {
+                array.splice(j, 1);
+            }
+        }
+    }
+    return array;
+   
+}
 // Do not edit below this line
 module.exports = removeFromArray;
