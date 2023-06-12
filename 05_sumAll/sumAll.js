1a84558ba9b344cb36c550491177fd63878d7dec
jorgeoliart <jorgeoliart.gmail.com>
2023-06-12 18:23:59 -0500

    Function works with any two positive intergers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    // declare variable for final sum
+    let finalSum = 0;
+
+    // condition: the first number is the lowest
+    if (num1 < num2) {
+        // include both num1 and num2 in the loop
+        for (i = num1; i <= num2; i++ ) {
+            // add the iterated value tu the final sum
+            sumAll = sumAll + i;
+        }
+    
+    // condition: the second number is the lowest
+    } else if (num2 < num1) {
+        // include both num2 and num1 in the loop
+        for (i = num2; i <= num1; i++) {
+            // add the iterated value to the final sum
+            sumAll = sumAll + i;
+        } 
+    }
+    return finalSum;
 
 };
 
