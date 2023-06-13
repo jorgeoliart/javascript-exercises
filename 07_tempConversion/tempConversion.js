35598440a8a9d66b40bf2fd761c241d0ce8d207e
jorgeoliart <jorgeoliart.gmail.com>
2023-06-13 07:27:33 -0500

    Round transformed numbers to have one decimal only

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -2,6 +2,7 @@ const convertToCelsius = function(inputFarenheit) {
   // transform Farenheit number into Celsius
   let celsius = (inputFarenheit - 32) * 5 / 9;
   // round Celsius number to have one decimal only
+  celsius = Math.round(celsius * 10) / 10;
   // return Celsius number
 };
 
@@ -9,6 +10,7 @@ const convertToFahrenheit = function(inputCelsius) {
   // transform Celsius number into Farenheit
   let farenheit = ((inputCelsius * 9 / 5) + 32);
   // round Farenheit number to have one decimal only
+  farenheit = Math.round(farenheit * 10) / 10;
   // return Farenheit number
 };
 
