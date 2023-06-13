afa199cf1418d25cc13b7d6d4b9006ca15454caf
jorgeoliart <jorgeoliart.gmail.com>
2023-06-13 07:24:10 -0500

    Transform celsius annd farenheit with formulas

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
 const convertToCelsius = function(inputFarenheit) {
   // transform Farenheit number into Celsius
+  let celsius = (inputFarenheit - 32) * 5 / 9;
   // round Celsius number to have one decimal only
   // return Celsius number
 };
 
 const convertToFahrenheit = function(inputCelsius) {
   // transform Celsius number into Farenheit
+  let farenheit = ((inputCelsius * 9 / 5) + 32);
   // round Farenheit number to have one decimal only
   // return Farenheit number
 };
