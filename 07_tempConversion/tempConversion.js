4c261b75bb4f7e0408f619b6e6a16b3cfcc04f78
jorgeoliart <jorgeoliart.gmail.com>
2023-06-13 07:20:41 -0500

    Write seudocode for both functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
 const convertToCelsius = function(inputFarenheit) {
+  // transform Farenheit number into Celsius
+  // round Celsius number to have one decimal only
+  // return Celsius number
 };
 
 const convertToFahrenheit = function(inputCelsius) {
+  // transform Celsius number into Farenheit
+  // round Farenheit number to have one decimal only
+  // return Farenheit number
 };
 
 // Do not edit below this line
