b2a81b397befad62ef67ae6c3a734b287c034690
jorgeoliart <jorgeoliart.gmail.com>
2023-06-12 20:56:20 -0500

    Do the leap years function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,11 @@
-const leapYears = function() {
+const leapYears = function(inputYear) {
+    let leapOrNotLeap;
+    if (((inputYear % 4 === 0) && (inputYear % 100 != 0)) || (inputYear % 400 === 0)) {
+        leapOrNotLeap = true;
+    } else {
+        leapOrNotLeap = false;
+    }
+    return leapOrNotLeap;
 
 };
 
